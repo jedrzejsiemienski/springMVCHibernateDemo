304631b7b26d5a0bfc5e35b9df351deee135dea7
jenczuen <jenczuen@gmail.com>
2016-02-01 07:47:27 +0100

    added possession data file used while passing possession data from form

---
+++ src/main/java/com/smartsoft/corpowarehousedemo/model/PossessionData.java
@@ -0,0 +1,20 @@
+package com.smartsoft.corpowarehousedemo.model;
+
+public class PossessionData {
+	private long itemId;
+	private long employeeId;
+
+	public long getItemId() {
+		return itemId;
+	}
+	public void setItemId(long itemId) {
+		this.itemId = itemId;
+	}
+	public long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(long employeeId) {
+		this.employeeId = employeeId;
+	}
+
+}
